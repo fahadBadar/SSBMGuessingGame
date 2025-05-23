08c1da0a6a88fb9c554ac6c34244d39aefa38297
fahad <fahad.m.badar@gmail.com>
2025-05-23 19:22:39 +0100

    feat: add outside click hook

---
+++ src/hooks/useOutsideClick.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+
+export function useOutsideClick({ ref, handler }) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        handler(event);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
+    };
+  }, [ref, handler]);
+}
