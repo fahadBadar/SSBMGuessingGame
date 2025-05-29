40c7d0e0022b21872d9542e587b883f150569675
fahad <fahad.m.badar@gmail.com>
2025-05-29 20:16:46 +0100

    feat: display game introduced and character universe category

--- src/components/displayGuessedCharacters.jsx
+++ src/components/displayGuessedCharacters.jsx
@@ -14,6 +14,12 @@ function DisplayGuessedCharacters({guessedCharacters, randomCharacter, character
                 <span className="w-16 text-center border-b">
                     Tier
                 </span>
+                <span className="w-16 text-center border-b">
+                    Introduced
+                </span>
+                <span className="w-16 text-center border-b">
+                    Universe
+                </span>
             </div>}
             <ul className={'w-full flex flex-col gap-2'}>
                 {guessedCharacters.map((character, index) => {
@@ -47,6 +53,18 @@ function DisplayGuessedCharacters({guessedCharacters, randomCharacter, character
                                 className={`flex items-center justify-center w-16 h-16 text-white border-2 ${characterInfo.tier === randomCharacterInfo.tier ? "bg-green-500" : "bg-red-500"} `}>
                                 <div className={'text-xs text-center'}>
                                     {characterInfo.tier}
+                                </div>
+                            </div>
+                            <div
+                                className={`flex items-center justify-center w-16 h-16 text-white border-2 ${characterInfo.gameIntroduced === randomCharacterInfo.gameIntroduced ? "bg-green-500" : "bg-red-500"} `}>
+                                <div className={'text-xs text-center'}>
+                                    {characterInfo.gameIntroduced}
+                                </div>
+                            </div>
+                            <div
+                                className={`flex items-center justify-center w-16 h-16 text-white border-2 ${characterInfo.universe === randomCharacterInfo.universe ? "bg-green-500" : "bg-red-500"} `}>
+                                <div className={'text-xs text-center'}>
+                                    {characterInfo.universe}
                                 </div>
                             </div>
                         </li>
