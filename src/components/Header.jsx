6d02b264d20fd13e46f66e5f25c17c2ad7c4fd0b
fahad <fahad.m.badar@gmail.com>
2026-09-15 17:33:38 +0100

    feat: Header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import {Component} from "react";
 export class Header extends Component {
     render() {
         return <div className="grid justify-items-center border-x-2 border-b-2 border-white">
-            <h1 className="p-2 justify-self-center text-gray-200 font-['SSBMFont']">SSBMdle</h1>
+            <div className="p-2 flex text-red-700 font-['SSBMFont'] text-5xl">
+                <span>SSBM</span>
+                <span className="text-white">dle</span>
+            </div>
         </div>
     }
 }
